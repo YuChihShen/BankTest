538557a969c7bad9ed67db6411d78d9ba09b6a39
SamShen <sam@weibyapps.com>
2024-01-12 12:24:37 +0800

    Initial Commit

---
+++ NewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewApp
+//
+//  Created by Yu-Chih Shen on 2024/1/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
